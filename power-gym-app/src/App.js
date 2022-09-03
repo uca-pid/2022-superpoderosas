40fefa6e1c8a80ccfb2dc14861084d700126a167
MaleBurs <maleburs@gmail.com>
2022-09-03 10:30:47 -0300

    Auth0 Integrated

--- power-gym-app/src/App.js
+++ power-gym-app/src/App.js
@@ -1,10 +1,24 @@
-import IntroductionPage from "./Introduction Page/IntroductionPage";
-import LogIn from "./Log In/LogIn";
+
+import LoginButton from "./Components/Registration Buttons/loginButton";
+import LogoutButton from "./Components/Registration Buttons/logoutButton";
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Profile } from './Components/Profile/Profile';
 
 export default function App() {
+  const { isAuthenticated } = useAuth0();
   return (
-   <>
-    <LogIn></LogIn>
-   </>
-  )
+  <div className="App">
+      <header className="App-header">
+        {isAuthenticated ? (
+          <>
+            <Profile />
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+      </header>
+    </div>
+  );
 }
