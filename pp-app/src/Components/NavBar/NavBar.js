1444235116bbb5214525f1849ae3642be97e5d54
MaleBurs <maleburs@gmail.com>
2022-09-07 16:03:53 -0300

    Final changes at navBar

--- pp-app/src/Components/NavBar/NavBar.js
+++ pp-app/src/Components/NavBar/NavBar.js
@@ -21,8 +21,8 @@ export default function NavBar() {
   return (
           <Popover>
               <nav className="bg-transparent container sm:h-10 fix mx-auto z-20 top-0 left-0 px-4 pt-6 sm:px-6 lg:px-8 pt-10" aria-label="Global">
-                <div className="mt-8 mx-auto flex flex-row justify-around md:mx-8">
-                    <div className="flex flex-row justify-between w-full mx-3 md:mx-0 md:basis-2/5">
+                <div className="mt-8 mx-auto flex flex-row justify-around lg:mx-8">
+                    <div className="flex flex-row justify-between w-full mx-3 lg:mx-0 lg:basis-2/5">
                         <div className=''>
                             <a href="https://patapila.org/index.php">
                             <img
@@ -32,13 +32,13 @@ export default function NavBar() {
                             />
                             </a>
                         </div>
-                        <div className="mt-5 flex items-center block mobile-nav-toggle md:hidden">
+                        <div className="mt-5 flex items-center block mobile-nav-toggle lg:hidden">
                             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-transparent p-2 text-gray-400 hover:text-gray-500">
                                 <FontAwesomeIcon icon="fa-bars" className="h-10 w-10 mobile-nav-toggle" aria-hidden="true"/>
                             </Popover.Button>
                         </div>
                     </div>
-                  <div className="justify-end mx-auto flex-1 hidden md:basis-3/5 md:pr-4 md:pt-8 md:flex mf:flex-row">
+                  <div className="justify-end mx-auto flex-1 hidden lg:basis-3/5 lg:pr-4 lg:pt-8 lg:flex lg:flex-row">
                   {navigation.map((item) => (
                     <a key={item.name} href={item.href} className='navBarText mt-2'>
                       {item.name}
@@ -61,7 +61,7 @@ export default function NavBar() {
             >
               <Popover.Panel
                 focus
-                className="absolute inset-x-0 top-5 z-10 origin-top-right transform p-2 transition md:hidden drop-shadow-[35px_35px_20px_rgba(0,0,0,0.4)]"
+                className="backdrop absolute h-full top-0 inset-x-0 z-10 origin-top-right transform p-2 transition lg:hidden"
               >
                 
                 <div className="px-5 pt-4 justify-self-end grid ">
@@ -71,18 +71,18 @@ export default function NavBar() {
                 </div>
                   
                 <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
-                  <div className="space-y-1 px-2 pt-2 pb-3">
+                  <div className="space-y-1 px-2 pt-2">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="block rounded-md px-3 py-2 text-base "
+                        className="block rounded-md px-3 py-4 text-base popUpText"
                       >
                         {item.name}
                       </a>
                     ))}
                   </div>
-                    <LoginButton style = {"block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100"}></LoginButton>
+                    <LoginButton style = {"block w-full px-5 py-4 text-center popUpButton"}></LoginButton>
                 </div>
               </Popover.Panel>
             </Transition>
