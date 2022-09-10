54b4eec57feec61dc46cdf969cdd2f5d054fcabb
MaleBurs <maleburs@gmail.com>
2022-09-10 17:30:01 -0300

    Update PictureWithText.js

--- pp-app/src/Components/Utiles/PictureWithText.js
+++ pp-app/src/Components/Utiles/PictureWithText.js
@@ -7,14 +7,14 @@ const  PictureWithText = ({ picture, title, text }) => {
   return (
     <>  
       <div className="greenBg py-5 md:py-20 lg:px-20 flex space-y-2 flex-col lg:flex-row ">  
-        <div className="grid justify-items-center basis-1/2 ml-40">
+        <div className="grid justify-items-center basis-1/2 mx-10 lg:ml-40">
           <img
-                className="object-scale-down h-200 w-auto"
+                className="object-scale-down h-200 w-auto mt-10"
                 src={picture}
                 alt="title"
           />
         </div>
-        <div className="basis-1/2 uneteTitle mx-40">
+        <div className="basis-1/2 uneteTitle mx-10 lg:mx-40">
           <div className="mt-5">
             {title}
           </div>
