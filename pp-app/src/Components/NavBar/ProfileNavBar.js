2fc5a64188f1efa4c306c31bbfddac74f06a67ac
paulaaletti <82065887+paulaaletti@users.noreply.github.com>
2022-09-18 18:03:02 -0300

    ProfileNavBar changes

--- pp-app/src/Components/NavBar/ProfileNavBar.js
+++ pp-app/src/Components/NavBar/ProfileNavBar.js
@@ -7,6 +7,7 @@ import "./profileNavBar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Buttons from "../Utiles/Butttons";
 import LogoPataPila_Colores from "../Images/LogoPataPila_Colores.jpg";
+import avatar from "../Images/avatar.jpeg";
 
 const navigation = [
   { name: '¿Por qué desnutrición?', href: 'https://patapila.org/desnutricion.php' },
@@ -15,8 +16,10 @@ const navigation = [
   { name: 'Involucrate', href: 'https://patapila.org/involucrate.php' },
 ]
 
-export default function ProfileNavBar() {
+export default function ProfileNavBar(props) {
   const navigate = useNavigate();
+  const userName = props.currentUser.name;
+  const userLastName = props.currentUser.lastname;
   function navigateToLogIn(){
       navigate("/login");
       window.location.reload();
@@ -26,8 +29,8 @@ export default function ProfileNavBar() {
     <>
           <Popover>
               <nav className="bg-transparent container sm:h-10 fix mx-auto z-20 top-0 left-0 px-4 sm:px-6 lg:px-8 pt-1" aria-label="Global">
-                <div className="mt-8 mx-auto flex flex-row justify-start lg:mx-8">
-                    <div className="flex flex-row justify-start w-full mx-3 lg:mx-0 lg:basis-1/7">
+                <div className="mx-auto flex flex-row justify-start lg:mt-5">
+                    <div className="justify-start flex mx-1 lg:mx-0 lg:basis-1/7">
                         <div className=''>
                             <a href="https://patapila.org/index.php">
                             <img
@@ -37,23 +40,30 @@ export default function ProfileNavBar() {
                             />
                             </a>
                         </div>
-                        <div className="mt-5 flex items-center block mobile-nav-toggle lg:hidden">
+                        <div className="mt-6 flex items-center block mobile-nav-toggle lg:hidden">
                             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-transparent p-2 text-gray-400 hover:text-gray-500">
                                 <FontAwesomeIcon icon="fa-bars" className="h-10 w-10 mobile-nav-toggle" aria-hidden="true"/>
                             </Popover.Button>
                         </div>
                     </div>
-                  <div className="justify-start mx-auto flex-1 hidden lg:basis-5/7 lg:pr-16 lg:flex lg:flex-row">
+                  <div className="justify-start mx-auto hidden lg:basis-5/7 lg:pr-20 lg:flex lg:flex-row mt-5">
                   {navigation.map((item) => (
                     <a key={item.name} href={item.href} className='navBarText mt-2'>
                       {item.name}
                     </a>
                   ))}
                   </div>
+                  <div className="flex justify-end lg:basis-1/7 -space-x-2 overflow-hidden mt-5 mx-auto flex-1 lg:pr-20 lg:flex lg:flex-row">
+                    <img className="flex inline-block h-9 w-9 rounded-full ring-2 ring-white" src={avatar} alt="avatar"></img>
+                    <div className='flex pl-5 userNameText mt-2'>
+                      {userName} {userLastName}
+                    </div>
+                    <div className="pl-5 justify-end inline-block mt-3 flex">
+                    <FontAwesomeIcon icon="fa-solid fa-angle-down" className="arrow-down-icon" />
+                    </div>
+                  </div>
                 </div>
               </nav>
-           
-
             <Transition
               as={Fragment}
               enter="duration-150 ease-out"
@@ -91,9 +101,6 @@ export default function ProfileNavBar() {
               </Popover.Panel>
             </Transition>
           </Popover>
-          <div className="flex justify-end lg:basis-1/7 -space-x-2 overflow-hidden">
-           <img className="inline-block h-9 w-9 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
-          </div>
           </>
   )
 }
--- pp-app/src/Pages/Profile Page/ProfilePage.js
+++ pp-app/src/Pages/Profile Page/ProfilePage.js
@@ -14,7 +14,7 @@ const Profile = () => {
   return (
     <>
       <div className="mx-auto relative z-10 pb-8 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32 h-screen bg-cover place-content-center">
-        <ProfileNavBar></ProfileNavBar> 
+        <ProfileNavBar currentUser={currentUser}></ProfileNavBar> 
     {currentUser ? (
     <div className="container">
       <header className="jumbotron">
