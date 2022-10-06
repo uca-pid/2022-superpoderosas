56d15d6ca7231c609ef30b003a729531dca50e7e
paulaaletti <82065887+paulaaletti@users.noreply.github.com>
2022-10-06 09:29:40 -0300

    Update ModifyStatePopUp.js

--- pp-app/src/Components/Forms/DonationModification/ModifyStatePopUp.js
+++ pp-app/src/Components/Forms/DonationModification/ModifyStatePopUp.js
@@ -3,15 +3,30 @@ import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { Popover } from '@headlessui/react';
 import DonationService from '../../../services/donations.service';
 import { useCurrentUser } from '../../../Context/CurrentUserContext';
+import ModalWithConfirmation from "../../Utiles/ModalWithConfirmation";
+import Modal from "../../Utiles/Modal";
+import { useState } from 'react'
 
 const ModifyStatePopUp = ( ) => {
     const {subscriptionData} = useCurrentUser()
+    const [showModalWithConfirmation, setShowModalWithConfirmation] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [cancellationRequest, setCancellationRequest] = useState(false);
+
+    const closeModalWithConfirmation = () => {
+      setShowModalWithConfirmation(false);
+      setCancellationRequest(false);
+    };
+  
+    const closeModal = () => {
+      setShowModal(false);
+      window.location.reload();
+    };
 
     const handleCancelledSubs = () =>{
         DonationService.modifySubscriptionState(subscriptionData.id, 'C').then(
             () => {
-              alert("Se cancelo")
-              window.location.reload();
+              setShowModal(true);
             },
             (error) => {
               const resMessage =
@@ -26,8 +41,7 @@ const ModifyStatePopUp = ( ) => {
     const handlePausedSubs = () =>{
         DonationService.modifySubscriptionState(subscriptionData.id, 'P').then(
             () => {
-              alert("Se pauso");
-              window.location.reload();
+              setShowModal(true);
             },
             (error) => {
               const resMessage =
@@ -41,6 +55,17 @@ const ModifyStatePopUp = ( ) => {
     }
     return (
       <>
+    {showModalWithConfirmation ? (
+      <ModalWithConfirmation value={showModalWithConfirmation} onChange={closeModalWithConfirmation} header={(!cancellationRequest) ?
+      "¿Estás seguro de que deseas pausar tu donación recurrente?"  :  "¿Estás seguro de que deseas cancelar tu donación recurrente?"
+          } body={(!cancellationRequest) ? "Si guardas los cambios, tu donación actual se pausará."
+              :   "Si guardas los cambios, tu donación recurrente se cancelará."} saveChanges={(!cancellationRequest) ? handlePausedSubs : handleCancelledSubs}></ModalWithConfirmation>
+    ) : null}
+    {showModal ? (
+      <Modal value={showModal} onChange={closeModal} header={(!cancellationRequest) ?
+      "Tu donación ha sido pausada con éxito!"  :  "Tu donación ha sido cancelada con éxito!"
+          }body={""} buttonText={"Continuar"}></Modal>
+    ) : null}
         <Popover>
             <Popover.Button className="justify-self-end lightgreyBgTranslucentHover rounded-3xl md:rounded-xl lg:basis-2/7 w-fit py-2 px-5">
             <div className="justify-center flex z-50 space-x-4 overflow-hidden mx-auto lg:flex-row">
@@ -49,11 +74,11 @@ const ModifyStatePopUp = ( ) => {
             </Popover.Button>
             <Popover.Panel className={"absolute mt-4 p-3 rounded-lg almostWhiteBg grayBorder space-y-2"}>
                 <div className="space-y-2">
-                <button onClick={handleCancelledSubs} className='w-full text-left z-50 relative font-Pop-M text-gray-400 duration-700 hover:text-gray-500 focus:text-gray-500 tracking-[0.5px] grayBottomBorder block px-1 py-2 text-base '>
+                <button onClick={()=>{setCancellationRequest(true);setShowModalWithConfirmation(true)}} className='w-full text-left z-50 relative font-Pop-M text-gray-400 duration-700 hover:text-gray-500 focus:text-gray-500 tracking-[0.5px] grayBottomBorder block px-1 py-2 text-base '>
                     Cancelar Subscripción
                 </button>
                 {(subscriptionData.subscriptionState.state !== 'P') ?
-                <button onClick={handlePausedSubs} className='w-full text-left z-50 relative font-Pop-M text-gray-400 duration-700 hover:text-gray-500 focus:text-gray-500 font-Pop-M tracking-[0.5px] block px-1 py-2 text-base'>
+                <button onClick={()=>{setShowModalWithConfirmation(true)}} className='w-full text-left z-50 relative font-Pop-M text-gray-400 duration-700 hover:text-gray-500 focus:text-gray-500 font-Pop-M tracking-[0.5px] block px-1 py-2 text-base'>
                     Pausar Subscripción
                 </button>
                 : null
