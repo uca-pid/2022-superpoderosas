a4a8c9079d5ac772270731d60183cd83e61ab264
MaleBurs <maleburs@gmail.com>
2022-10-25 16:05:57 -0300

    Adding Fa Icon to Change password

--- pp-app/src/Components/Forms/PasswordModificationForms/PasswordModificationsViaEmail/ChangePasswordForm.js
+++ pp-app/src/Components/Forms/PasswordModificationForms/PasswordModificationsViaEmail/ChangePasswordForm.js
@@ -11,6 +11,7 @@ import Modal from "../../../Utiles/Modal"
 import ValidationFunctions from "../../../../functions/validations";
 import "../../../../App.css"
 import Messages from "../../Messages";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ChangePasswordForm(props)  {
   const form = useRef();
@@ -19,8 +20,17 @@ export default function ChangePasswordForm(props)  {
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [message, setMessage] = useState("");
+  const [passwordShown1, setPasswordShown1] = useState(false);
+  const [passwordShown2, setPasswordShown2] = useState(false);
   const navigate = useNavigate();
   const params = useParams();
+
+  const togglePassword1 = () => {
+    setPasswordShown1(!passwordShown1);
+  };
+  const togglePassword2 = () => {
+    setPasswordShown2(!passwordShown2);
+  };
 
   const continuePostNavigationSuccessful = () =>{
     setShowModal(true);
@@ -99,26 +109,31 @@ export default function ChangePasswordForm(props)  {
                 </div>
           </div>
 
-            <div className="space-y-2 rounded-md mt-2">       
+            <div className="space-y-2 rounded-md mt-2">
+              <div className= "relative flex flex-row justify-between bg-transparent h-auto block w-full rounded-xl border border-gray-300 px-2 focus:z-10 focus:outline-none greenBorderWhenFocus form-control">       
                 <Input
-                  type="password"
-                  className="relative bg-transparent block w-full rounded-xl border border-gray-300 px-6 py-2 text-gray-900 placeholder-gray-600 focus:z-10 font-Pop-R text-xs tracking-[0.5px] focus:outline-none greenBorderWhenFocus form-control"
+                  type={(!passwordShown1) ? "password" : "text"}
+                  className="bg-transparent block w-full border-transparent py-2 text-gray-900 placeholder-red focus:border-transparent focus:ring-0 font-Pop-R text-sm focus:outline-none"
                   name="password"
                   value={password}
                   placeholder="Contraseña nueva"
                   onChange={onChangePassword}
                   validations={[ValidationFunctions.required, ValidationFunctions.vpassword]}
                 />
+                <span className="pt-2 px-2"><FontAwesomeIcon onClick={togglePassword1} icon={passwordShown1? "fa-solid fa-eye-slash": "fa-solid fa-eye"} color='#000'size={14} /></span>
+              </div>
+              <div className= "relative flex flex-row justify-between bg-transparent h-auto block w-full rounded-xl border border-gray-300 px-2 focus:z-10 focus:outline-none greenBorderWhenFocus form-control">
                 <Input
-                  type="password"
-                  className="relative bg-transparent block w-full rounded-xl border border-gray-300 px-6 py-2 text-gray-900 placeholder-gray-600 focus:z-10 font-Pop-R text-xs tracking-[0.5px] focus:outline-none greenBorderWhenFocus form-control"
+                  type={(!passwordShown2) ? "password" : "text"}
+                  className="bg-transparent block w-full border-transparent py-2 text-gray-900 placeholder-red focus:border-transparent focus:ring-0 font-Pop-R text-sm focus:outline-none"
                   name="password2"
                   value={password2}
                   placeholder="Confirmar contraseña"
                   onChange={onChangePassword2}
                   validations={[ValidationFunctions.required]}
                 />
-              
+                <span className="pt-2 px-2"><FontAwesomeIcon onClick={togglePassword2} icon={passwordShown2? "fa-solid fa-eye-slash": "fa-solid fa-eye"} color='#000'size={14} /></span>
+              </div>
             </div>
     
             <Buttons.SolidGreenButton text={"Guardar cambios"} color={"purpleBg"} margins={"my-6 md:my-6"} onClick={null}/>
