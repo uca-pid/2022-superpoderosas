6d8adc6e13f721ad0a9eeb46a463266dbd113d19
paulaaletti <82065887+paulaaletti@users.noreply.github.com>
2022-11-04 15:10:41 -0300

    Confirm Cancellation by writing

--- pp-app/src/Components/Forms/Donations/DonationModification/ModifyStatePopUp.js
+++ pp-app/src/Components/Forms/Donations/DonationModification/ModifyStatePopUp.js
@@ -4,17 +4,24 @@ import { Popover } from '@headlessui/react';
 import DonationService from '../../../../services/donations.service';
 import { useCurrentUser } from '../../../../Context/CurrentUserContext';
 import ModalWithConfirmationAndDetails from "../../../Utiles/ModalWithConfirmationAndDetails";
+import ModalWithWrittenConfirmation from "../../../Utiles/ModalWithWrittenConfirmation"
 import Modal from "../../../Utiles/Modal";
 import { useState } from 'react'
 
 const ModifyStatePopUp = ( ) => {
+  
     const {subscriptionData} = useCurrentUser()
     const [showModalWithConfirmation, setShowModalWithConfirmation] = useState(false);
+    const [showModalWithWrittenConfirmation, setShowModalWithWrittenConfirmation] = useState(false);
     const [showModal, setShowModal] = useState(false);
     const [cancellationRequest, setCancellationRequest] = useState(false);
 
     const closeModalWithConfirmation = () => {
       setShowModalWithConfirmation(false);
+    };
+
+    const closeModalWithWrittenConfirmation = () => {
+      setShowModalWithWrittenConfirmation(false);
     };
   
     const closeModal = () => {
@@ -57,12 +64,19 @@ const ModifyStatePopUp = ( ) => {
       <>
     {showModalWithConfirmation ? (
     <ModalWithConfirmationAndDetails 
-    value={showModalWithConfirmation} onChange={closeModalWithConfirmation} header={(!cancellationRequest) ?
-      "¿Estás seguro de que deseas pausar tu donación recurrente?"  :  "¿Estás seguro de que deseas cancelar tu donación recurrente?"
-          } body={(!cancellationRequest) ? "Si guardas los cambios, tu donación actual se pausará."
-              :   "Si guardas los cambios, tu donación recurrente se cancelará."} saveChanges={(!cancellationRequest) ? handlePausedSubs : handleCancelledSubs}
-    action={(!cancellationRequest) ? "pausará" : "cancelará"}></ModalWithConfirmationAndDetails>
+    value={showModalWithConfirmation} onChange={closeModalWithConfirmation} header={
+      "¿Estás seguro de que deseas pausar tu donación recurrente?"
+          } body={"Si guardas los cambios, tu donación actual se pausará."} saveChanges={handlePausedSubs}
+    action={ "pausará"}></ModalWithConfirmationAndDetails>
     ) : null}
+    { showModalWithWrittenConfirmation ? (
+      <ModalWithWrittenConfirmation 
+    value={showModalWithConfirmation} onChange={closeModalWithWrittenConfirmation} header={"¿Estás seguro de que deseas cancelar tu donación recurrente?"
+          } body={"Si guardas los cambios, tu donación recurrente se cancelará."} saveChanges={handleCancelledSubs}
+    action={"cancelará"}></ModalWithWrittenConfirmation>
+    ) : null
+
+    }
     {showModal ? (
       <Modal value={showModal} onChange={closeModal} header={(!cancellationRequest) ?
       "Tu donación ha sido pausada con éxito!"  :  "Tu donación ha sido cancelada con éxito!"
@@ -76,7 +90,7 @@ const ModifyStatePopUp = ( ) => {
             </Popover.Button>
             <Popover.Panel className={"absolute top-0 right-0 mt-4 p-3 rounded-lg almostWhiteBg grayBorder space-y-2"}>
                 <div className="space-y-2">
-                <button onClick={()=>{setCancellationRequest(true);setShowModalWithConfirmation(true)}} className='w-full text-left z-50 relative text-gray-400 duration-700 font-Pop-M hover:text-gray-500 focus:text-gray-500 tracking-[0.5px] grayBottomBorder block px-1 py-2 text-xs'>
+                <button onClick={()=>{setCancellationRequest(true);setShowModalWithWrittenConfirmation(true)}} className='w-full text-left z-50 relative text-gray-400 duration-700 font-Pop-M hover:text-gray-500 focus:text-gray-500 tracking-[0.5px] grayBottomBorder block px-1 py-2 text-xs'>
                     Cancelar Subscripción
                 </button>
                 {(subscriptionData.subscriptionState.state !== 'P') ?
